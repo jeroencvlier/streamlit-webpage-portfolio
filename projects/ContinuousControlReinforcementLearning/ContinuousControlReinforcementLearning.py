0452e298f6e7c65ee3f5182451c480c268b5dec0
Jeroen <jeroencvlier@gmail.com>
2023-08-08 19:36:46 +0200

    project updates

--- projects/ContinuousControlReinforcementLearning/ContinuousControlReinforcementLearning.py
+++ projects/ContinuousControlReinforcementLearning/ContinuousControlReinforcementLearning.py
@@ -1,3 +1,31 @@
 from pages.utils import *
 
+st.set_page_config(
+    initial_sidebar_state="collapsed",
+    page_title="Jeroen | RL - Continuous Control!",
+    page_icon=im,
+)
+
 load_css()
+title_header("Continuous Control Reinforcement Learning", line=True)
+this_project = project_folder / "ContinuousControlReinforcementLearning"
+
+import numpy as np
+
+
+# --------------------------------------------------------------
+# Project Description
+# --------------------------------------------------------------
+
+with open(f"{this_project}/project_readme.md", "r") as f:
+    project_readme = f.read()
+
+
+with st.container():
+    st.markdown(project_readme, unsafe_allow_html=True)
+
+# --------------------------------------------------------------
+# Project Image
+# --------------------------------------------------------------
+
+st.image(f"{this_project}/ContinuousControl.gif") 
--- projects/Multi-agentReinforcementLearning/Multi-agentReinforcementLearning.py
+++ projects/Multi-agentReinforcementLearning/Multi-agentReinforcementLearning.py
@@ -1,2 +1,29 @@
 from pages.utils import *
+
+st.set_page_config(
+    initial_sidebar_state="collapsed",
+    page_title="Jeroen | RL - MultiAgent!",
+    page_icon=im,
+)
+
 load_css()
+title_header("Multi-agent RL", line=True)
+this_project = project_folder / "Multi-agentReinforcementLearning"
+
+
+# --------------------------------------------------------------
+# Project Description
+# --------------------------------------------------------------
+
+with open(f"{this_project}/project_readme.md", "r") as f:
+    project_readme = f.read()
+
+
+with st.container():
+    st.markdown(project_readme, unsafe_allow_html=True)
+
+# --------------------------------------------------------------
+# Project Image
+# --------------------------------------------------------------
+st.write("##")
+st.image(f"{this_project}/Tennis.gif")
