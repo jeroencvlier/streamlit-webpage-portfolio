d71a4626a14afcda875e519fec53e3572ba148f9
Jeroen <jeroencvlier@gmail.com>
2023-08-10 19:05:17 +0200

    www

--- og_tags_component/og_tags.py
+++ og_tags_component/og_tags.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 
 def og_tags():
     _component_func = components.declare_component(
-        "og_tags", url="https://wwwjeroencvlier.com"
+        "og_tags", url="https://www.jeroencvlier.com"
     )
 
     return _component_func()
